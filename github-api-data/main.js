bae0353d87fe8e76cbf9ebec82037f335420e5b5
nonluck <luiskilmerluk1221@gmail.com>
2023-02-19 21:24:14 -0300

    github api

--- github-api-data/main.js
+++ github-api-data/main.js
@@ -1,4 +1,26 @@
-var languages=[]
+
+var total=0;
+var resumedlanguages=[];
+var languages=[];
+function resetvalues(){
+    total=0;
+    resumedlanguages=[];
+    anguages=[];
+}
+function counterusability(data){
+    if (resumedlanguages.find(item => item.language === data.language)){
+        for(let i=0; i < resumedlanguages.length;i++){
+            if(resumedlanguages[i].language==data.language){
+                resumedlanguages[i].usability += data.usability;
+            }
+        }
+        total += data.usability;
+    }else{
+        resumedlanguages.push(data)
+        total += data.usability;
+    }
+}
+
 async function get_names(name){
     return fetch(`https://api.github.com/users/${name}/repos`)
 }
@@ -7,25 +29,58 @@ async function language_for_repo(user, repo){
     .then(res =>{set_languages(res)})
 
 }
+
 function set_languages(lang){
-    console.log(lang);
-    languages.push(lang);console.log(languages);
+    
+    Object.keys(lang).forEach(element => {
+        const aux = {language:`${element}`,usability:lang[element]}
+        languages.push(aux);
+        counterusability(aux)
+        
+    }
+    )
+    
 }
 
 function search_diretory(user,answer){
-    var listName=[]
     answer.forEach(element => {
         language_for_repo(user,element.name)
-    });
+    })
 }
-async function get_github(){
-    const user = "HanielCostaDaSilva";
+
+function sendName(){
+    const user = document.getElementById("name").value
+    document.getElementById("name").value = ''
+    get_github(user)
+}
+
+async function get_github(user){
+    //const user = "HanielCostaDaSilva";
+    resetvalues();
     let aux = (await get_names(user)).json()
-    aux.then(total =>{search_diretory(user,total)})
+    await aux.then(total =>{search_diretory(user,total)})
+    .catch(e => console.log(e+"deu ruim"))
+    loadingPages();
+    setTimeout(function() {
+        showData(user);
+      }, 500);
 }
-     
-console.log(get_github())
 
+function showData(user){
+    const root = document.getElementById("root")
+    root.innerHTML = `<div id="user">${user}</div>`;
+    resumedlanguages.forEach(content =>{
+        console.log(content)
+        let aux = root.innerHTML;
+        const aux2 = `<div >${content.language} : ${(content.usability/total*100).toFixed(2)}%</div>`
+        aux+=aux2;
+        root.innerHTML = aux;
+    })
+}
 
+function loadingPages(){
+    const loading = `<div> <i class="fa fa-spinner"></i></div>`
+    document.getElementById("root").innerHTML = loading;
+}
 
 //https://api.github.com/repos/nonluck/competitive-programming/languages
